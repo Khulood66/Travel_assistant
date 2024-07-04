9fc5042da40cf97fc2d88743192539efb5868cf1
khulood khaled <KhuloodKhaled97@gmail.com>
2024-07-04 23:45:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,140 +1,10 @@
-# import streamlit as st
-# from openai import OpenAI
-
-# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-
-# st.sidebar.header("Sbet parameters")
-# sys_Mesaage=st.sidebar.text_input("System message:", value="You are helpful assistant")
-# model_TEM=st.sidebar.slider(label="Temp", step=0.01, min_value=0.0,max_value=2.0,value=1.0)
-# number = st.sidebar.number_input("Insert a number", value=None, placeholder="Type a number...")
-# # st.write("The current number is ", number)
-
-# def get_gym_program(weight, tall):
-#   response = client.chat.completions.create(
-#       model="gpt-3.5-turbo",
-#       temperature=1.7,
-#       messages=[
-#           {"role": "system", "content": f"{sys_Mesaage}"},
-#           {"role": "user", "content": f">>Bio stats: {weight}, {tall}"},
-#       ]
-#     )
-#   return response.choices[0].message.content
-
-# st.title("Real title")
-# st.markdown("**Day1 : Upper Body**")
-# st.write(sys_Mesaage)
-
-
-
-# import streamlit as st
-# from openai import OpenAI
-
-# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-
-# def get_gym_program(system_message, weight, tall, goal):
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         temperature=0.7,
-#         messages=[
-#             {"role": "system", "content": f"{system_message}"},
-#             {"role": "user", "content": f">>Bio stats: {weight}, {tall}\n\n>>Objective: {goal}"},
-#         ]
-#     )
-#     return response.choices[0].message.content
-
-
-# # User inputs and model parameters
-# st.sidebar.header("Sbet parameters")
-# system_message = st.sidebar.text_input("System message:",
-#                                        value="You are helpful assistant")
-# model_temp = st.sidebar.slider("Temp", step=0.01, min_value=0.0,
-#                                max_value=2.0,
-#                                value=1.0)
-# max_token = st.sidebar.slider("Max Token", step=100, min_value=200,
-#                               max_value=4000,
-#                               value=512)
-
-# goal = st.sidebar.text_input("User objective:",
-#                              value="General fitness")
-# weight = st.sidebar.number_input(
-#     "Insert your weight", value=70, placeholder="Your weight in KG")
-# tall = st.sidebar.number_input(
-#     "How tall are you:", value=165, placeholder="Your length in CM")
-
-
-# # Main page components
-# st.title("🥗 Sbet the nutritionist")
-# get_gym_program(system_message, weight, tall, goal)
-
-
-# st.markdown("*Day 1: Upper Body*")
-
-
-# import streamlit as st
-# from openai import OpenAI
-
-# client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-
-# def get_gym_program(system_message, weight, tall, goal):
-#     response = client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         temperature=0.7,
-#         messages=[
-#             {"role": "system", "content": f"{system_message}"},
-#             {"role": "user", "content": f">>Bio stats: {weight}, {tall}\n\n>>Objective: {goal}"},
-#         ]
-#     )
-#     return response.choices[0].message.content
-
-
-# # User inputs and model parameters
-# st.sidebar.header("Sbet parameters")
-# prompt_message = "You are Sbet my GYM Trainer. \
-#                     Based on my objective and my \
-#                     Bio stats give me one week program. \
-#                     Then add to it nutritionist advise for \
-#                     the type of food and calories I need to consume."
-
-# system_message = st.sidebar.text_area("System message:",
-#                                       value=prompt_message)
-# model_temp = st.sidebar.slider("Temp", step=0.01, min_value=0.0,
-#                                max_value=2.0,
-#                                value=1.0)
-# max_token = st.sidebar.slider("Max Token", step=100, min_value=200,
-#                               max_value=4000,
-#                               value=512)
-
-# st.sidebar.header("User based input:")
-# goal = st.sidebar.text_input("User objective:",
-#                              value="General fitness")
-# weight = st.sidebar.number_input(
-#     "Insert your weight", value=70, placeholder="Your weight in KG")
-# tall = st.sidebar.number_input(
-#     "How tall are you:", value=165, placeholder="Your length in CM")
-
-
-# # Main page components
-# st.title("🥗 Sbet the nutritionist")
-# response = get_gym_program(system_message, weight, tall, goal)
-
-
-# st.markdown(response)
-
 
 import streamlit as st
 import openai
-import folium
-from streamlit_folium import folium_static
-# st.set_config('browser.uiDirection', 'RTL')
 st.set_page_config('Travale assites',page_icon='.\Vector.png',)
 
-# إعدادات OpenAI
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# وظائف مساعدة
 def get_destination_suggestions(preferences, budget,lan):
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -156,7 +26,6 @@ def get_activity_hotels_suggestions(destination,lan):
             {"role": "user", "content": f"Suggest popular tourist activities and places to visit in {destination} for a budget of {budget} and can you give some pictures to thes places."}
         ]
     )
-    print(response.choices[0].message.content)
     return response.choices[0].message.content
 
 def get_accommodation_suggestions(destination, budget,lan):
@@ -217,16 +86,13 @@ translations = {
     }
 }
 
-# اختيار اللغة
+# Choose language
 language = st.sidebar.selectbox("اختر اللغة | Choose Language", ["English", "العربية"])
 lang_code = 'en' if language == "English" else 'ar'
 
-
-
-# واجهة المستخدم باستخدام Streamlit
 st.title(translations[lang_code]['title'])
 
-# إدخال تفضيلات المستخدم
+# inputs from users
 preferences = st.sidebar.text_area(translations[lang_code]['preferences'])
 budget = st.sidebar.number_input(translations[lang_code]['budget'], min_value=0)
 destination = st.sidebar.text_input(translations[lang_code]['destination'])
@@ -248,24 +114,8 @@ if st.sidebar.button(translations[lang_code]['suggest_destinations']):
             st.write(plan)
             destinations = get_destination_suggestions(preferences, budget,language)
             st.subheader(translations[lang_code]['destination_suggestions'])
-            st.markdown(body=destinations,unsafe_allow_html=True
-)
+            st.markdown(body=destinations,unsafe_allow_html=True)
 
-        # destination_list = [line.strip() for line in destinations.split('\n') if line.strip()]
-
-        # destination = st.selectbox(translations[lang_code]['choose_destination'], destination_list)
-        
-
-            # عرض الخريطة التفاعلية
-            # st.subheader(translations[lang_code]['map'])
-            
-            # # تعيين إحداثيات افتراضية (على سبيل المثال، وسط المدينة)
-            # coordinates = [25.276987, 55.296249]  # إحداثيات افتراضية لدبي، يمكنك تغييرها حسب الحاجة
-
-            # map_center = coordinates
-            # m = folium.Map(location=map_center, zoom_start=13)
-            # folium.Marker(location=map_center, popup=destination).add_to(m)
-            # folium_static(m)
     else:
         st.error(translations[lang_code]['error_details'])
 
